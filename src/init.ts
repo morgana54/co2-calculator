f876237d7feccf6c6f3c8eb9be7f40d9752c4cce
Jakub Pietrzak <jpietrzi@gmail.com>
2022-07-05 15:31:43 +0200

    display bubble chart based on changing input value

--- src/init.ts
+++ src/init.ts
@@ -1,19 +1,42 @@
+import { renderGraph } from "./bubbleChart.js";
+
 // get to constants
 const SLIDERS_IDS = ["netflix", "spotify"] as const;
 
 type TSliderId = typeof SLIDERS_IDS[number];
 
 // Application state (POTEM TS polepszyć)
-const componentValues = {
+const slidersValues = {
   netflix: 0,
   spotify: 0,
 };
 
 // JUŻ MASZ BIEŻACE WARTOŚCI INPUTOW W STANIE
 
-// const calculateBubblesData = () => {
-//   return [{}];
-// };
+// TERAZ: wyświetlenie wartości inputow w kolkach
+
+// skmiń dobrą nazwę!!
+const getFile = (id: string, sliderValue: number) => {
+  // MUSISZ USTALIĆ JAK DOKŁADNIE MA WYGLĄDAĆ JEDEN PLIK (JAKIE PROPERTISY)
+  // I CO KAŻDY Z NICH ROBI, A JAK NIE ZROZUMIESZ TO ZAPYTASZ KACPRA
+  return { id: id, value: sliderValue };
+};
+
+const calculateBubblesData = () => {
+  // tutaj na podstawie stanu slidersValues STWORZYSZ PLIK FILES
+
+  // nazwa do zmiany
+  const files = [];
+  for (const id in slidersValues) {
+    // TODO: kalkulacja CO2
+
+    // add eac calculated co2 value
+
+    // ts do naprawienia
+    files.push(getFile(id, slidersValues[id]));
+  }
+  return files;
+};
 
 // const renderGraph = (files) => {
 
@@ -25,9 +48,11 @@ const handleSliderChange = (sliderId: TSliderId) => (event: Event) => {
 
   console.log(newValue);
 
-  componentValues[sliderId] = newValue as unknown as number;
+  slidersValues[sliderId] = newValue as unknown as number;
 
-  // renderGraph(calculateBubblesData(componentValues));
+  console.log(calculateBubblesData());
+
+  renderGraph(calculateBubblesData());
 };
 
 // wziac to do osobnego pliku ładnie potem
