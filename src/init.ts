f74fc5e22af3223c1da1bd9adc3b68a5d1b9630a
Jakub Pietrzak <jpietrzi@gmail.com>
2022-07-05 09:44:07 +0200

    init the project

---
+++ src/init.ts
@@ -0,0 +1,29 @@
+// const componentValues = {
+//   netlix: 0,
+//   spotify: 0,
+// };
+
+// const calculateBubblesData = () => {
+//   return [{}];
+// };
+
+// const renderGraph = (files) => {
+
+// }
+
+// const sliderOnChange = (sliderId) => (event) => {
+//     const newValue = event.target.value
+
+//     componentValues[sliderId] = newValue;
+
+//     renderGraph(calculateBubblesData(componentValues));
+// }
+
+// const slidersId = ['netflix', 'spotify']
+
+// const $slidersMap = slidersId.reduce((map, sliderId) => {
+//     const $slider = document.getElementById(sliderId);
+//     map.add(sliderId, $slider);
+
+//     $slider.addEventListener('change', sliderOnChange(sliderId))
+// }, new Map())
